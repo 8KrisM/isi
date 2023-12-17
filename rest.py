3cf484061ada5ec6a1e6c3d63b0531aac95d33c2
Kristián Michalik <kristian.michalik@student.tuke.sk>
2023-12-17 10:38:09 +0100

    apina pipina cors

--- rest.py
+++ rest.py
@@ -2,11 +2,21 @@ from typing import List
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import utils
 
 from algorithms import BFS
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Item(BaseModel):
     maze: List[List[str]]
